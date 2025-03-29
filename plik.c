0550fb7f54045de1605411d41a88aabbe44b6a7d
oliwia <01197800@pw.edu.pl>
2025-03-29 18:39:32 +0100

    Dodałam dwie struktury na krawędzie i sam graf, przy czym zmieniłam też trochę sposób odczytywania pliku

--- plik.c
+++ plik.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void readFile (const char *input, int isBinary){
-    FILE *file = fopen(input, isBinary? "rb" : "r");
+#define MAX_NODES 100
+
+typedef struct{
+    int u, v;
+}Edge;
+
+typedef struct{
+    int numNodes, numEdges;
+    Edge edges[MAX_NODES * MAX_NODES]; //Liczba krawędzi
+}Graph;
+
+Graph readFile (const char *input){
+    Graph g;
+    FILE *file = fopen(input, "r");
     if(file == NULL){
         perror("Nie można otworzyć pliku\n");
         return;
+    }
+
+    fprintf(file, "%d %d\n", g.numNodes, g.numEdges);
+    for (int i = 0; i < g.numEdges; i++) {
+        fprintf(file, "%d %d\n", g.edges[i].u, g.edges[i].v);
     }
     fclose(file);
 }
