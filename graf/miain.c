f667a2ad627bebace192666bd59a5ea0cc179211
Oliwia <01197800@gmail.com>
2025-04-06 15:46:07 +0200

    Dodano algorytm BFS

--- graf/miain.c
+++ graf/miain.c
@@ -2,4 +2,20 @@
 int main()
 {
 	wczytajGraf("graftestsigma.txt");
+	Graf* g = wczytajGraf("graf.txt");
+
+    if (czySpojny(g)) {
+        printf("Graf jest spójny.\n");
+    } else {
+        printf("Graf NIE jest spójny.\n");
+    }
+
+    saveText("wynik.txt", g);
+    saveBin("wynik.bin", g);
+
+    // Zwolnij pamięć itp. jak chcesz
+    free(g->tablicaWezlow);
+    free(g);
+    return 0;
+
 }
--- graf/test.c
+++ graf/test.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <string.h>
+#include <stdbool.h>
 #include "graf.h"
 
 
@@ -103,4 +104,50 @@ void saveBin(const char* nazwaPliku, const Graf* graf) {
 
     fclose(plik);
     printf("Graf został zapisany do pliku binarnego: %s\n", nazwaPliku);
+}
+
+bool czySpojny(Graf* graf) {
+    if (!graf || graf->liczbaWezlow <= 0) return false;
+
+    bool* odwiedzone = (bool*)calloc(graf->liczbaWezlow, sizeof(bool));
+    if (!odwiedzone) {
+        printf("Błąd alokacji pamięci.\n");
+        return false;
+    }
+
+    int* kolejka = (int*)malloc(graf->liczbaWezlow * sizeof(int));
+    int front = 0, tyl = 0;
+
+    kolejka[tyl++] = 0;
+    odwiedzone[0] = true;
+
+    while (front < tyl) {
+        int aktualny = kolejka[front++];
+
+        for (int i = 0; i < graf->liczbaWierszy; i++) {
+            int sasiad = -1;
+            if (graf->tablicaWezlow[i].a == aktualny) {
+                sasiad = graf->tablicaWezlow[i].b;
+            } else if (graf->tablicaWezlow[i].b == aktualny) {
+                sasiad = graf->tablicaWezlow[i].a;
+            }
+
+            if (sasiad != -1 && !odwiedzone[sasiad]) {
+                odwiedzone[sasiad] = true;
+                kolejka[tyl++] = sasiad;
+            }
+        }
+    }
+
+    bool spojny = true;
+    for (int i = 0; i < graf->liczbaWezlow; i++) {
+        if (!odwiedzone[i]) {
+            spojny = false;
+            break;
+        }
+    }
+
+    free(odwiedzone);
+    free(kolejka);
+    return spojny;
 }
